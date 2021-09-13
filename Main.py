c01918920b7b0b2402b3fc741fe4cf00a4abd32d
Ratman457 <90657738+Ratman457@users.noreply.github.com>
2021-09-13 17:36:30 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,13 @@
+'''
+TODO Add 'time' system (make travel cost time) (add day tracker)
+TODO Create inventory system for a player
+TODO Add inventories for shops (add shops)
+TODO Expand travel system to include events (add events)
+TODO Create combat system
+TODO Create quest system
+'''
+
+
 from Location_Functions import initialize
 from Location_Functions import in_town
 
